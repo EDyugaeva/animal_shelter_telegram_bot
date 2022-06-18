63b968282214dd847c1bd43369a849f74a078790
EDyugaeva <ed2408@yandex.ru>
2022-06-18 15:20:49 +0300

    Initial commit

---
+++ src/main/java/pro/sky/animal_shelter_telegram_bot/AnimalShelterTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.animal_shelter_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/animal_shelter_telegram_bot/AnimalShelterTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.animal_shelter_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
