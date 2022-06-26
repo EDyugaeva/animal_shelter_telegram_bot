b09eec249ea36155cf1e5d28583e6d7cd353acd3
avvasil <avvasil@mail.ru>
2022-06-26 19:28:41 +0300

    1. Remote postgresql server account properties are encrypted application.properties file 2. changelog-master.yml file created 3. migrations.sql file created and DB schemas are added

--- src/main/java/pro/sky/animal_shelter_telegram_bot/model/User.java
+++ src/main/java/pro/sky/animal_shelter_telegram_bot/model/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Objects;
+
 @Entity
 public class User {
 
---
+++ src/main/resources/liquibase/scripts/migrations.sql
@@ -0,0 +1,17 @@
+-- liquibase formatted sql
+
+-- changeset avvasil:1
+CREATE TABLE animals
+(
+    id          serial          NOT NULL PRIMARY KEY,
+    animal_name varchar(255)    NOT NULL,
+    animal_age  integer
+);
+
+CREATE TABLE users
+(
+    id              serial          NOT NULL PRIMARY KEY,
+    user_firstname  varchar(255)    NOT NULL,
+    user_lastname   varchar(255)    NOT NULL
+);
+
