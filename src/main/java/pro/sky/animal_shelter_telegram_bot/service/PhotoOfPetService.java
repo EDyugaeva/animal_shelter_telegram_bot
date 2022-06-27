c3e1d9a441d49cc172ff2b8869187cde3ea03007
EDyugaeva <ed2408@yandex.ru>
2022-06-27 21:30:15 +0300

    Update service PhotoOfPetService.java from Branch_Kate

---
+++ src/main/java/pro/sky/animal_shelter_telegram_bot/service/PhotoOfPetService.java
@@ -0,0 +1,16 @@
+package pro.sky.animal_shelter_telegram_bot.service;
+
+import org.springframework.web.multipart.MultipartFile;
+import pro.sky.animal_shelter_telegram_bot.model.pets.PhotoOfPet;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface PhotoOfPetService {
+
+    void uploadPhotoOfPet(Long reportId, MultipartFile avatarFile) throws IOException;
+
+    void downloadPhotoOfPet(Long reportId, HttpServletResponse response) throws IOException;
+
+    PhotoOfPet findPhotoByReportId(Long reportId);
+}
