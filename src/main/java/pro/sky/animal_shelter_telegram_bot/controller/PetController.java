acdc03c349164939258afa8b29b5f3f34eb29856
EDyugaeva <ed2408@yandex.ru>
2022-06-28 20:54:15 +0300

    Add CRUD methods in PetController, without service

--- src/main/java/pro/sky/animal_shelter_telegram_bot/controller/PetController.java
+++ src/main/java/pro/sky/animal_shelter_telegram_bot/controller/PetController.java
@@ -4,14 +4,23 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pro.sky.animal_shelter_telegram_bot.model.pets.Pet;
+
+import java.util.Collection;
 
 
 @RestController
 @RequestMapping("/pet")
 public class PetController {
+
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
 
     @ApiResponses({
             @ApiResponse(
@@ -27,5 +36,34 @@ public class PetController {
                 "2. get pet information\n" +
                 "2. update pet information\n" +
                 "4. remove pet information\n";
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Pet> findStudent(@PathVariable Long id) {
+        Pet pet = petService.findPet(id);
+        if (pet == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(pet);
+    }
+
+    @PostMapping
+    public Pet addPet(@RequestBody Pet pet) {
+        return petService.addPet(pet);
+    }
+
+    @PutMapping
+    public ResponseEntity<Pet> editPet(@RequestBody Pet pet) {
+        Pet editPet = petService.changePet(pet);
+        if (editPet == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.ok(pet);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Pet> deletePet(@PathVariable Long id) {
+        petService.deletePet(id);
+        return ResponseEntity.ok().build();
     }
 }
