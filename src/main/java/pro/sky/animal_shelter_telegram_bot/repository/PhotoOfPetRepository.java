025d704e8acd01b63fc2bb5979fcff0d98a367fb
SergeFox02 <Fox2188@gmail.com>
2022-07-04 17:12:40 +0300

    Update PetController

--- src/main/java/pro/sky/animal_shelter_telegram_bot/repository/PhotoOfPetRepository.java
+++ src/main/java/pro/sky/animal_shelter_telegram_bot/repository/PhotoOfPetRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.animal_shelter_telegram_bot.model.pets.PhotoOfPet;
 
-import java.util.Collection;
-
 @Repository
 public interface PhotoOfPetRepository extends JpaRepository<PhotoOfPet, Long> {
 
