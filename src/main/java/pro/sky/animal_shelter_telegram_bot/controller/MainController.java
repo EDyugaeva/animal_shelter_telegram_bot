de7c88ba6ad6c8f2c34578052871e53402480422
SergeFox02 <Fox2188@gmail.com>
2022-07-05 15:48:24 +0300

    Update MainController.java

--- src/main/java/pro/sky/animal_shelter_telegram_bot/controller/MainController.java
+++ src/main/java/pro/sky/animal_shelter_telegram_bot/controller/MainController.java
@@ -1,22 +1,28 @@
 package pro.sky.animal_shelter_telegram_bot.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Send a welcome message",
-                    content = @Content(mediaType = "string", examples = @ExampleObject(value = "example of string"))
-            )
-    })
+    @Operation(
+            summary = "Welcome message in our pet shelter",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Send a welcome message",
+                            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE,
+                                    examples = @ExampleObject(value = "string"))
+                    )
+            },
+            tags = "Pet shelter (home page)"
+    )
     @GetMapping
     public String helloMessage() {
         return "Welcome to our pet shelter!";
