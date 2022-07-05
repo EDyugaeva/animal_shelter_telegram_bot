7c9916cafd6736cdd87fb5c4d8eec04150441ceb
SergeFox02 <Fox2188@gmail.com>
2022-07-05 13:02:51 +0300

    Update PhotoOfPetRepository from Branch_Kate

--- src/main/java/pro/sky/animal_shelter_telegram_bot/repository/PhotoOfPetRepository.java
+++ src/main/java/pro/sky/animal_shelter_telegram_bot/repository/PhotoOfPetRepository.java
@@ -4,7 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.animal_shelter_telegram_bot.model.pets.PhotoOfPet;
 
+import java.util.Collection;
+import java.util.Optional;
+
 @Repository
 public interface PhotoOfPetRepository extends JpaRepository<PhotoOfPet, Long> {
 
+    void deleteById(Long id);
+
+    Collection<PhotoOfPet> findPhotoOfPetByReportId(Long id);
+
+    Optional<PhotoOfPet> findByReportId(Long reportId);
 }
