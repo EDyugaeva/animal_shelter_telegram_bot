c69e4cc4d814979b9bd7e1a337638651ebd7dcf4
SergeFox02 <Fox2188@gmail.com>
2022-07-09 16:41:43 +0300

    Add method uploadPhotoOfPet in ReportController

--- src/main/java/pro/sky/animal_shelter_telegram_bot/controller/ReportController.java
+++ src/main/java/pro/sky/animal_shelter_telegram_bot/controller/ReportController.java
@@ -6,18 +6,27 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import pro.sky.animal_shelter_telegram_bot.model.Report;
+import pro.sky.animal_shelter_telegram_bot.service.PhotoOfPetService;
 import pro.sky.animal_shelter_telegram_bot.service.ReportService;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/report")
 public class ReportController {
 
     private final ReportService reportService;
+    private final PhotoOfPetService photoOfPetService;
+
+    Logger logger = LoggerFactory.getLogger(PhotoOfPetService.class);
 
     private final String HELLO_MESSAGE = "You can do it by reports\n" +
             "1. add new report\n" +
@@ -25,9 +34,11 @@ public class ReportController {
             "2. update report\n" +
             "4. remove report\n";
 
-    public ReportController(ReportService reportService) {
+    public ReportController(ReportService reportService, PhotoOfPetService photoOfPetService) {
         this.reportService = reportService;
+        this.photoOfPetService = photoOfPetService;
     }
+
 
     @Operation(
             summary = "Welcome message",
@@ -94,6 +105,37 @@ public class ReportController {
     @PostMapping
     public Report addReport(@RequestBody Report report) {
         return reportService.addReport(report);
+    }
+
+    @Operation(
+            summary = "Add photo of pet to report",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Add photo to report",
+                    content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)
+            ),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Add photo",
+                            content = @Content(
+                                    mediaType = MediaType.IMAGE_JPEG_VALUE
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Add photo is to big"
+                    )
+            },
+            tags = "Reports"
+    )
+    @PostMapping(value = "/{id}/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> upLoadPhotoOfPet(@PathVariable Long id, @RequestParam MultipartFile photo) throws IOException {
+        if (photo.getSize() > 1024 * 300) {
+            logger.warn("Warning: photo is to big");
+            return ResponseEntity.badRequest().body("File is to big");
+        }
+        photoOfPetService.uploadPhotoOfPet(id, photo);
+        return ResponseEntity.ok().build();
     }
 
     @Operation(
