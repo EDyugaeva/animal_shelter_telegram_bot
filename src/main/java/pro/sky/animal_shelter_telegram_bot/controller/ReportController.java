1968eb1f7990c297a197c80ff3830428957d13ba
SergeFox02 <Fox2188@gmail.com>
2022-07-09 16:55:50 +0300

    Add method findPhotoByReportId in ReportController

--- src/main/java/pro/sky/animal_shelter_telegram_bot/controller/ReportController.java
+++ src/main/java/pro/sky/animal_shelter_telegram_bot/controller/ReportController.java
@@ -8,16 +8,18 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pro.sky.animal_shelter_telegram_bot.model.Report;
+import pro.sky.animal_shelter_telegram_bot.model.pets.PhotoOfPet;
 import pro.sky.animal_shelter_telegram_bot.service.PhotoOfPetService;
 import pro.sky.animal_shelter_telegram_bot.service.ReportService;
 
-import java.io.IOException;
+import java.io.*;
 
 @RestController
 @RequestMapping("/report")
@@ -38,7 +40,6 @@ public class ReportController {
         this.reportService = reportService;
         this.photoOfPetService = photoOfPetService;
     }
-
 
     @Operation(
             summary = "Welcome message",
@@ -81,6 +82,30 @@ public class ReportController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         return ResponseEntity.ok(report);
+    }
+
+    @Operation(
+            summary = "Find photo by reportId",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Found photo:",
+                            content = @Content(mediaType = MediaType.IMAGE_JPEG_VALUE)
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "If photo not found"
+                    )
+            },
+            tags = "Reports"
+    )
+    @GetMapping(value = "/{id}/photo")
+    public ResponseEntity<byte[]> findPhotoByReportId(@PathVariable Long id) {
+        PhotoOfPet photoOfPet = photoOfPetService.findPhotoByReportId(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(photoOfPet.getMediaType()));
+        headers.setContentLength(photoOfPet.getData().length);
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(photoOfPet.getData());
     }
 
     @Operation(
