a4eec8c2f417c22d0fc7c348cfc53f37664840ba
Serge <Fox2188@gmail.com>
2022-07-18 22:09:36 +0300

    Create PetControllerTestMVC is work.

---
+++ src/test/java/pro/sky/animal_shelter_telegram_bot/controller/PetControllerTestMVC.java
@@ -0,0 +1,148 @@
+package pro.sky.animal_shelter_telegram_bot.controller;
+
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pro.sky.animal_shelter_telegram_bot.model.pets.Pet;
+import pro.sky.animal_shelter_telegram_bot.repository.PetRepository;
+import pro.sky.animal_shelter_telegram_bot.service.impl.PetServiceImpl;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static pro.sky.animal_shelter_telegram_bot.controller.ConstantsForControllerTests.*;
+
+@WebMvcTest(controllers = PetController.class)
+public class PetControllerTestMVC {
+
+    private final String LOCAL_URL = URL + PORT + "/" + PET_URL + "/";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PetRepository petRepository;
+
+    @SpyBean
+    private PetServiceImpl petService;
+
+    @InjectMocks
+    private PetController petController;
+
+    private final Pet PET = new Pet();
+    private final long ID = 1L;
+    private final String NAME_OF_PET = "Barsik";
+    private final String HEALTH = "Health is nice";
+    private final String EXTRA_INFO_OF_PET = "This is a dog";
+    private final JSONObject petOwnerObject = new JSONObject();
+
+    @BeforeEach
+    private void StartData() {
+        petOwnerObject.put("id", ID);
+        petOwnerObject.put("nameOfPet", NAME_OF_PET);
+        petOwnerObject.put("health", HEALTH);
+        petOwnerObject.put("extraInfoOfPet", EXTRA_INFO_OF_PET);
+        PET.setId(ID);
+        PET.setNameOfPet(NAME_OF_PET);
+        PET.setHealth(HEALTH);
+        PET.setExtraInfoOfPet(EXTRA_INFO_OF_PET);
+    }
+
+    @Test
+    public void testFindPet() throws Exception{
+        when(petRepository.findById(any(Long.class))).thenReturn(Optional.of(PET));
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get(LOCAL_URL + ID)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(ID))
+                .andExpect(jsonPath("$.nameOfPet").value(NAME_OF_PET))
+                .andExpect(jsonPath("$.health").value(HEALTH))
+                .andExpect(jsonPath("$.extraInfoOfPet").value(EXTRA_INFO_OF_PET));
+    }
+
+    @Test
+    public void testFindPetIfNotFound() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(LOCAL_URL + ID)
+                        .accept(MediaType.TEXT_PLAIN_VALUE))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testAddPetOwner() throws Exception {
+        when(petRepository.save(any(Pet.class))).thenReturn(PET);
+        when(petRepository.findById(any(Long.class))).thenReturn(Optional.of(PET));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post(LOCAL_URL)
+                        .content(petOwnerObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(ID))
+                .andExpect(jsonPath("$.nameOfPet").value(NAME_OF_PET))
+                .andExpect(jsonPath("$.health").value(HEALTH))
+                .andExpect(jsonPath("$.extraInfoOfPet").value(EXTRA_INFO_OF_PET));
+    }
+
+    @Test
+    public void testEditPet() throws Exception {
+        when(petRepository.save(any(Pet.class))).thenReturn(PET);
+        when(petRepository.findById(any(Long.class))).thenReturn(Optional.of(PET));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put(LOCAL_URL)
+                        .content(petOwnerObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(ID))
+                .andExpect(jsonPath("$.nameOfPet").value(NAME_OF_PET))
+                .andExpect(jsonPath("$.health").value(HEALTH))
+                .andExpect(jsonPath("$.extraInfoOfPet").value(EXTRA_INFO_OF_PET));
+    }
+
+    @Test
+    public void testEditPetIfBadRequest() throws Exception {
+        when(petRepository.findById(any(Long.class))).thenReturn(null);
+        mockMvc.perform(MockMvcRequestBuilders.put(LOCAL_URL + ID)
+                        .accept(MediaType.TEXT_PLAIN_VALUE))
+                .andExpect(status().is(405));
+    }
+
+    @Test
+    public void testDeletePetOwner() throws Exception {
+        when(petRepository.save(any(Pet.class))).thenReturn(PET);
+        when(petRepository.findById(any(Long.class))).thenReturn(Optional.of(PET));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete(LOCAL_URL + ID)
+                        .content(petOwnerObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(ID))
+                .andExpect(jsonPath("$.nameOfPet").value(NAME_OF_PET))
+                .andExpect(jsonPath("$.health").value(HEALTH))
+                .andExpect(jsonPath("$.extraInfoOfPet").value(EXTRA_INFO_OF_PET));
+    }
+
+    @Test
+    public void testDeletePetOwnerIfNotFound() throws Exception {
+        when(petRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+        mockMvc.perform(MockMvcRequestBuilders.delete(LOCAL_URL + ID)
+                        .accept(MediaType.TEXT_PLAIN_VALUE))
+                .andExpect(status().isNotFound());
+    }
+}
